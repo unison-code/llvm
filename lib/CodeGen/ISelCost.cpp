d93eaee27365f3177662fcfb8f2efbc9fdd9c9d6
Roberto Castaneda Lozano <rcas@sics.se>
2015-04-09 14:51:23 +0200

    Completed the pass to estimate the number of cycles

--- lib/CodeGen/ISelCost.cpp
+++ lib/CodeGen/ISelCost.cpp
@@ -12,22 +12,31 @@
 //
 //===----------------------------------------------------------------------===//
 
-#define DEBUG_TYPE "expand-isel-pseudos"
+#define DEBUG_TYPE "isel-cost"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/TargetSchedule.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Metadata.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Target/TargetLowering.h"
 #include "llvm/Target/TargetMachine.h"
+
 using namespace llvm;
 
 namespace {
+
   class ISelCost : public MachineFunctionPass {
   public:
     static char ID; // Pass identification, replacement for typeid
     ISelCost() : MachineFunctionPass(ID) {}
 
   private:
+
+    TargetSchedModel SchedModel;
+    const TargetInstrInfo *TII;
+
     virtual bool runOnMachineFunction(MachineFunction &MF);
 
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
@@ -44,21 +53,31 @@ INITIALIZE_PASS(ISelCost, "isel-cost",
 
 bool ISelCost::runOnMachineFunction(MachineFunction &MF) {
 
+  TII = MF.getTarget().getInstrInfo();
+
+  const TargetSubtargetInfo &ST =
+    MF.getTarget().getSubtarget<TargetSubtargetInfo>();
+  SchedModel.init(*ST.getSchedModel(), &ST, TII);
+
+  int cycles = 0;
   int size = 0;
 
   // Iterate through each instruction in the function
   for (MachineFunction::iterator I = MF.begin(), E = MF.end(); I != E; ++I) {
     MachineBasicBlock *MBB = I;
+    // TODO: check for null pointers and fail gracefully
+    const MDNode * fn =
+      MBB->getBasicBlock()->getTerminator()->getMetadata("exec_freq");
+    int f = ((const ConstantInt *)fn->getOperand(0))->getLimitedValue();
     for (MachineBasicBlock::iterator MBBI = MBB->begin(), MBBE = MBB->end();
          MBBI != MBBE; ) {
       MachineInstr *MI = MBBI++;
-
+      cycles +=  f * SchedModel.computeInstrLatency(MI);
       size += MI->getDesc().getSize();
-
     }
   }
 
-  errs() << "{\"size\": " << size << "}\n";
+  errs() << "{\n \"cycles\": " << cycles << ",\n \"size\": " << size << "\n}\n";
 
   return false;
 
